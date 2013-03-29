cbf42e22f78622cbbd0c1a299d99ca56eee9a7a9
collinbc <collinbc@rose-hulman.edu>
2013-03-29 13:15:53 -0400

    Help screens added. Need to link with segues

---
+++ TrackApp/TrackApp/HelpDocController.h
@@ -0,0 +1,13 @@
+//
+//  HelpDocController.h
+//  TrackApp
+//
+//  Created by Collins, Brian C on 3/29/13.
+//  Copyright (c) 2013 ATMA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelpDocController : UITableViewController
+
+@end
---
+++ TrackApp/TrackApp/HelpDocController.m
@@ -0,0 +1,42 @@
+//
+//  HelpDocController.m
+//  TrackApp
+//
+//  Created by Collins, Brian C on 3/29/13.
+//  Copyright (c) 2013 ATMA. All rights reserved.
+//
+
+#import "HelpDocController.h"
+
+@interface HelpDocController ()
+
+@end
+
+@implementation HelpDocController
+
+- (void)pop
+{
+    [super dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
